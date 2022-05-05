1bb6e1258b3a27eb0b1436e68a00935cc740eaac
HOBOT <sergeiismirnoff@gmail.com>
2022-05-05 12:32:01 +0300

    Initial commit

---
+++ AwesomeProgect/AwesomeProgectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeProgectApp.swift
+//  AwesomeProgect
+//
+//  Created by Сергей on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeProgectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
